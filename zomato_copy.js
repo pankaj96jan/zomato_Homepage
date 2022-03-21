1fb412bcdf2fb587691f9201d65b328e361977a5
pankaj96jan <99659418+pankaj96jan@users.noreply.github.com>
2022-03-21 17:55:43 +0530

    Add files via upload

---
+++ zomato_copy.js
@@ -0,0 +1,9 @@
+// console.log("this is zomato copy");
+let searchResult =document.getElementById("btn");
+searchResult.addEventListener("click",greet())
+function greet(){
+    console.log("hello");
+}
+// function greet(){
+//     console.log("hello");
+// }
